de02d8ea8d5fd96791e511ef61f6facfd18b1d25
evstorm <25943752+EvStorM@users.noreply.github.com>
2021-07-20 18:24:21 +0800

    ✨ fix(snippet): 修复部分错误snippet

--- src/test/test.vue
+++ src/test/test.vue
@@ -1,41 +1,17 @@
 <!--
  * @Date: 2021-07-19 10:36:33
  * @LastEditors: E'vils
- * @LastEditTime: 2021-07-20 18:09:30
+ * @LastEditTime: 2021-07-20 18:24:19
  * @Description: 
  * @FilePath: /src/test/test.vue
 -->
 
-<template>
-  <view class="" hover-class="none" hover-stop-propagation="false">
-    <image
-      class=""
-      src=""
-      mode="heightFix"
-      lazy-load="false"
-      @error="test"
-      @load="test"
-    >
-    </image>
-    <button
-      class="default"
-      size="default"
-      type="default"
-      plain="default"
-      disabled="false"
-      loading="false"
-      hover-class="button-hover"
-      hover-stop-propagation="false"
-      form-type="submit|reset"
-      open-type="openSetting"
-    ></button>
-  </view>
-</template>
+<template> </template>
 <script lang="ts">
 export default {
   data() {
     return {
-      alist: "",
+      list: "",
     };
   },
   methods: {
